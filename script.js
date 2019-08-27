359e49d8f7b9c1f1b385530a728e0804af101ed4
Alán Arámbula <arambulagar@gmail.com>
2019-08-26 23:45:10 -0600

    First stage Landing

---
+++ script.js
@@ -0,0 +1,94 @@
+'use strict'
+
+TweenMax.to(".overlay h1", 2, {
+    opacity: 0,
+    y: -60,
+    ease: Expo.easeInOut
+})
+
+TweenMax.to(".overlay", 2, {
+    delay: 1,
+    top: "-100%",
+    ease: Expo.easeInOut
+})
+
+TweenMax.from(".logo", 2, {
+    delay: 2.4,
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+})
+
+TweenMax.staggerFrom(".nav ul li", 1, {
+    delay: 2.4,
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+}, 0.2)
+
+TweenMax.staggerFrom(".social ul li", 1, {
+    delay: 2.4,
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+}, 0.2)
+
+TweenMax.from(".side-strip", 2, {
+    delay: 2.4,
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut
+})
+
+
+TweenMax.from(".row", 2, {
+    delay: 2.4,
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut
+})
+
+TweenMax.staggerFrom(".row h3", 1, {
+    delay: 2.4,
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+}, 0.2)
+
+TweenMax.staggerFrom(".row p", 1, {
+    delay: 2.4,
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+}, 0.3)
+
+TweenMax.from(".header h1", 2, {
+    delay: 3.2,
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+})
+
+TweenMax.from(".header h2", 3, {
+    delay: 3.2,
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+})
+
+TweenMax.from(".header p", 4, {
+    delay: 3.2,
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+})
+
+TweenMax.from(".header button", 5, {
+    delay: 3.2,
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+})
+
+
+
